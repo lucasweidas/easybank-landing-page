5e30186405ce9c9de8d24ff0256a629c67331f5b
lucasweidas <lucasweidas@gmail.com>
2023-07-19 11:14:38 -0300

    add initial styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import './globals.css';
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import { Public_Sans } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] });
+const publicSans = Public_Sans({ subsets: ['latin'], weight: 'variable', variable: '--font-public-sans' });
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${publicSans.variable} font-sans`}>{children}</body>
     </html>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,32 @@
+const { fontFamily } = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./pages/**/*.{js,ts,jsx,tsx,mdx}', './components/**/*.{js,ts,jsx,tsx,mdx}', './app/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-public-sans)', ...fontFamily.sans],
+      },
+      colors: {
+        blue: {
+          900: 'hsl(233, 26%, 24%)',
+        },
+        lime: {
+          450: 'hsl(136, 65%, 51%)',
+        },
+        cyan: {
+          450: 'hsl(192, 70%, 51%)',
+        },
+        gray: {
+          100: 'hsl(220, 16%, 96%)',
+          400: 'hsl(233, 8%, 62%)',
+        },
+      },
+      backgroundImage: {
+        'linear-gradient-1': 'linear-gradient(90deg, hsl(136, 65%, 51%), hsl(192, 70%, 51%))',
+      },
+    },
   },
   plugins: [],
 };
