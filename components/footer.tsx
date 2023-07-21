7e591d735f20d9ff48340871e9a728a78bac49cb
lucasweidas <lucasweidas@gmail.com>
2023-07-21 16:14:23 -0300

    finish Footer component - large version

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,8 +2,8 @@ import Link from 'next/link';
 
 export default function Footer() {
   return (
-    <footer className="bg-blue-900 px-4 xs:px-6 py-10">
-      <div className="grid gap-10 md:grid-cols-2 md:justify-items-start max-w-3xl mx-auto">
+    <footer className="bg-blue-900 px-4 xs:px-6 py-10 lg:px-0 lg:py-12">
+      <div className="grid gap-10 md:grid-cols-2 md:justify-items-start max-w-3xl mx-auto lg:max-w-none lg:w-min-lg lg:grid-cols-[auto_1fr_auto] lg:gap-32">
         <div className="flex flex-col gap-7 items-center md:items-start md:justify-between">
           <Link href="/" aria-label="Easybank Home">
             <svg className="w-[139px] h-[20px] text-white">
@@ -48,7 +48,7 @@ export default function Footer() {
             </li>
           </ul>
         </div>
-        <nav className="grid gap-5 md:grid-cols-2 md:col-start-2 md:justify-self-stretch text-center md:text-start" aria-label="Secondary">
+        <nav className="grid gap-5 md:grid-cols-2 md:justify-self-stretch text-center md:text-start lg:gap-20 lg:justify-self-start" aria-label="Secondary">
           <ul className="flex flex-col gap-5">
             <li className="leading-none">
               <Link href="#" className="inline-block text-white hover:text-lime-450 focus-visible:text-lime-450 transition-colors">
@@ -84,7 +84,7 @@ export default function Footer() {
             </li>
           </ul>
         </nav>
-        <div className="flex flex-col items-center gap-7 md:col-span-2 md:justify-self-center">
+        <div className="flex flex-col items-center gap-7 md:col-span-2 md:justify-self-center lg:col-span-1 lg:justify-self-end lg:items-end">
           <Link
             href="#"
             className="bg-gradient-to-r from-lime-450 to-cyan-450 text-white px-8 py-3.5 rounded-full inline-block leading-none hover:brightness-125 hover:grayscale-50 focus-visible:brightness-125 focus-visible:grayscale-50 transition-filter"
